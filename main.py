85428905d01fe296341beb89df5e87fe83fd2f7a
Henry Veedahl <89225651+HenryNerd@users.noreply.github.com>
2025-12-16 09:49:22 -0600

    Add Main.py to Git Tracking

---
+++ main.py
@@ -0,0 +1,43 @@
+import redis
+
+print('Battery Event Recorder')
+print('1: Battery Recived')
+print('2: Discharge Test')
+print('3: Deployed In Robot')
+print('4: Returned to Storage')
+print('')
+choice = input('$~ ')
+if choice == '1':
+    print('Enroll New Battery')
+    eventType = "0001"
+    season = input('Season Recived: ')
+    mounth = input('Month Recived: ')
+    friendlyName = input('Name: ')
+    batteryID = input('Battery ID: ')
+    capacity = input('Capacity (Ah): ')
+
+    events = redis.Redis(host='localhost', port=6969, decode_responses=True)
+
+    events.hset(eventType+"-"+batteryID, mapping={
+    'eventType': eventType,
+    'season': season,
+    'month': mounth,
+    'friendlyName': friendlyName,
+    'batteryID' : batteryID,
+    'capacity' : capacity
+    })
+    events.close()
+elif choice == '2':
+    print('Battery Discharge Test')
+    batterySearch = input("Battery ID")
+    try:
+        events = redis.Redis(host='localhost', port=6969, decode_responses=True)
+        data = events.hgetall('0001-'+batterySearch)
+        print()
+        print("Battery Found:")
+        print(data['friendlyName']+" | "+data['season']+" "+data['month'])
+    except:
+        print("An exception occurred") 
+    print()
+    mesuredah = input("Amps")
+
