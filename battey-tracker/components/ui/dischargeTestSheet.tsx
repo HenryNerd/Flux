0dff2c833f4b20e17580ce64be7978b5909d3985
Henry Veedahl <89225651+HenryNerd@users.noreply.github.com>
2025-12-26 21:17:18 -0600

    Add Mobile Layout For Discharge Testing

--- battey-tracker/components/ui/dischargeTestSheet.tsx
+++ battey-tracker/components/ui/dischargeTestSheet.tsx
@@ -13,6 +13,16 @@ import {
 } from "@/components/ui/sheet"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
 
 interface BatteryData {
     season: string
@@ -76,51 +86,103 @@ export default function DischargeTest({ battery }: { battery: string }) {
     }, [battery])
 
     return (
-        <Sheet>
-            <SheetTrigger asChild>
-                <Button>Discharge Test</Button>
-            </SheetTrigger>
-            <SheetContent>
-                <SheetHeader>
-                    <SheetTitle className="text-center">
-                        Add Discharge Test For {data?.friendlyName || 'Battery'}
-                    </SheetTitle>
-                </SheetHeader>
-                <form onSubmit={handleSubmit}>
-                    <div className="m-2">
-                        <div className='mt-3'>
-                            <Label className="mb-3" htmlFor="ah">Measured Ah</Label>
-                            <Input 
-                                value={mesuredAh} 
-                                onChange={(e) => setMesuredAh(e.target.value)} 
-                                id="ah" 
-                            />
-                        </div>
-                        <div className='mt-3'>
-                            <Label className="mb-3" htmlFor="wh">Measured Wh</Label>
-                            <Input 
-                                value={mesuredWh} 
-                                onChange={(e) => setMesuredWh(e.target.value)} 
-                                id="wh" 
-                            />
-                        </div>
-                        <div className='mt-3'>
-                            <Label className="mb-3" htmlFor="time">Test Time</Label>
-                            <Input 
-                                value={testTime} 
-                                onChange={(e) => setTestTime(e.target.value)} 
-                                id="time" 
-                                placeholder="H:MM:SS" 
-                            />
-                        </div>
-                    </div>
-                    <SheetFooter>
-                        <SheetClose asChild>
-                            <Button type='submit'>Add Event</Button>
-                        </SheetClose>
-                    </SheetFooter>
-                </form>
-            </SheetContent>
-        </Sheet>
+        <div>
+            <div className="block md:hidden">
+                <Drawer>
+                    <DrawerTrigger asChild>
+                        <Button>Discharge Test</Button>
+                    </DrawerTrigger>
+                    <DrawerContent>
+                        <DrawerHeader>
+                            <DrawerTitle className="text-center">
+                                Add Discharge Test For {data?.friendlyName || 'Battery'}
+                            </DrawerTitle>
+                        </DrawerHeader>
+                        <form onSubmit={handleSubmit}>
+                            <div className="m-2">
+                                <div className='mt-3'>
+                                    <Label className="mb-3" htmlFor="ah">Measured Ah</Label>
+                                    <Input
+                                        value={mesuredAh}
+                                        onChange={(e) => setMesuredAh(e.target.value)}
+                                        id="ah"
+                                    />
+                                </div>
+                                <div className='mt-3'>
+                                    <Label className="mb-3" htmlFor="wh">Measured Wh</Label>
+                                    <Input
+                                        value={mesuredWh}
+                                        onChange={(e) => setMesuredWh(e.target.value)}
+                                        id="wh"
+                                    />
+                                </div>
+                                <div className='mt-3'>
+                                    <Label className="mb-3" htmlFor="time">Test Time</Label>
+                                    <Input
+                                        value={testTime}
+                                        onChange={(e) => setTestTime(e.target.value)}
+                                        id="time"
+                                        placeholder="H:MM:SS"
+                                    />
+                                </div>
+                            </div>
+                            <DrawerFooter>
+                                <DrawerClose asChild>
+                                    <Button type='submit'>Add Event</Button>
+                                </DrawerClose>
+                            </DrawerFooter>
+                        </form>
+                    </DrawerContent>
+                </Drawer>
+            </div>
+            <div className="hidden md:block">
+                <Sheet>
+                    <SheetTrigger asChild>
+                        <Button>Discharge Test</Button>
+                    </SheetTrigger>
+                    <SheetContent>
+                        <SheetHeader>
+                            <SheetTitle className="text-center">
+                                Add Discharge Test For {data?.friendlyName || 'Battery'}
+                            </SheetTitle>
+                        </SheetHeader>
+                        <form onSubmit={handleSubmit}>
+                            <div className="m-2">
+                                <div className='mt-3'>
+                                    <Label className="mb-3" htmlFor="ah">Measured Ah</Label>
+                                    <Input
+                                        value={mesuredAh}
+                                        onChange={(e) => setMesuredAh(e.target.value)}
+                                        id="ah"
+                                    />
+                                </div>
+                                <div className='mt-3'>
+                                    <Label className="mb-3" htmlFor="wh">Measured Wh</Label>
+                                    <Input
+                                        value={mesuredWh}
+                                        onChange={(e) => setMesuredWh(e.target.value)}
+                                        id="wh"
+                                    />
+                                </div>
+                                <div className='mt-3'>
+                                    <Label className="mb-3" htmlFor="time">Test Time</Label>
+                                    <Input
+                                        value={testTime}
+                                        onChange={(e) => setTestTime(e.target.value)}
+                                        id="time"
+                                        placeholder="H:MM:SS"
+                                    />
+                                </div>
+                            </div>
+                            <SheetFooter>
+                                <SheetClose asChild>
+                                    <Button type='submit'>Add Event</Button>
+                                </SheetClose>
+                            </SheetFooter>
+                        </form>
+                    </SheetContent>
+                </Sheet>
+            </div>
+        </div>
     )
 }
