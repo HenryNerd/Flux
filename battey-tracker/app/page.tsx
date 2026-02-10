f1830e943aff9c2361b3b343397640a8f4c5a89a
Henry Veedahl <89225651+HenryNerd@users.noreply.github.com>
2026-02-09 19:59:25 -0600

    Add User Mangment UI and Start Work on Backend

--- battey-tracker/app/page.tsx
+++ battey-tracker/app/page.tsx
@@ -35,6 +35,17 @@ import {
   DialogTitle,
   DialogHeader
 } from "@/components/ui/dialog"
+import { Card } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge"
+import { Crown, Wrench } from "lucide-react"
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+import {
+  Field,
+  FieldContent,
+  FieldDescription,
+  FieldLabel,
+  FieldTitle,
+} from "@/components/ui/field"
 
 export default function Home() {
   const router = useRouter();
@@ -131,51 +142,102 @@ export default function Home() {
           </DialogContent>
         </Dialog>
       )}
-      <Drawer>
-        <DrawerTrigger asChild>
-          <Button className="bg-slate-200 text-color-black ml-4" variant="outline">QR Code Scanner</Button>
-        </DrawerTrigger>
-        <DrawerContent>
-          <DrawerTitle>Qr Code Scaner</DrawerTitle>
-          <Scanner
-            onScan={(result) => console.log(result)}
-            components={{
-              torch: true,
-              zoom: true,
-            }}
-          />
-        </DrawerContent>
-      </Drawer>
-      <Sheet>
-        <SheetTrigger asChild>
-          <Button className="bg-slate-200 mt-4 mb-5 ml-3 mr-3" variant="outline">New Battery</Button>
-        </SheetTrigger>
-        <SheetContent>
-          <SheetHeader>
-            <form onSubmit={handleSubmit}>
-              <Label htmlFor="season">Season Purchased: </Label>
-              <Input value={season} onChange={(e) => setseason(e.target.value)} id="season"></Input>
+      <Card className="m-4 p-4 bg-red-300">
+        <div className="flex gap-2">
+          <h1 className="text-4xl font-medium font-sans text-slate-100">Hey, Henry</h1>
+          <Badge className="m-2 bg-red-150 border-3 border-slate-100 text-slate-100"><Wrench></Wrench>Pit Member</Badge>
+          <Badge className="m-2 bg-red-150 border-3 border-slate-100 text-slate-100"><Crown></Crown>Pit Admin</Badge>
+        </div>
+        <div className="flex flex-wrap gap-2">
+          <Drawer>
+            <DrawerTrigger asChild>
+              <Button className="bg-slate-100 text-color-black" variant="outline">Scan Battery</Button>
+            </DrawerTrigger>
+            <DrawerContent>
+              <DrawerTitle>Qr Code Scaner</DrawerTitle>
+              <Scanner
+                onScan={(result) => console.log(result)}
+                components={{
+                  torch: true,
+                  zoom: true,
+                }}
+              />
+            </DrawerContent>
+          </Drawer>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button className="bg-slate-100" variant="outline">New Battery</Button>
+            </SheetTrigger>
+            <SheetContent>
+              <SheetHeader>
+                <form onSubmit={handleSubmit}>
+                  <Label htmlFor="season">Season Purchased: </Label>
+                  <Input value={season} onChange={(e) => setseason(e.target.value)} id="season"></Input>
 
-              <Label htmlFor="mounth">Mounth Purchased: </Label>
-              <Input value={mounth} onChange={(e) => setmounth(e.target.value)} id="mounth"></Input>
+                  <Label htmlFor="mounth">Mounth Purchased: </Label>
+                  <Input value={mounth} onChange={(e) => setmounth(e.target.value)} id="mounth"></Input>
 
-              <Label htmlFor="name">Battery Name: </Label>
-              <Input value={name} onChange={(e) => setname(e.target.value)} id="name"></Input>
+                  <Label htmlFor="name">Battery Name: </Label>
+                  <Input value={name} onChange={(e) => setname(e.target.value)} id="name"></Input>
 
-              <Label htmlFor="nfc">NFC Tag: </Label>
-              <Input value={nfc} onChange={(e) => setnfc(e.target.value)} id="nfc"></Input>
+                  <Label htmlFor="nfc">NFC Tag: </Label>
+                  <Input value={nfc} onChange={(e) => setnfc(e.target.value)} id="nfc"></Input>
 
-              <Label htmlFor="capacity">Battery Capaciy (Ah): </Label>
-              <Input value={capacity} onChange={(e) => setcapacity(e.target.value)} id="capacity"></Input>
+                  <Label htmlFor="capacity">Battery Capaciy (Ah): </Label>
+                  <Input value={capacity} onChange={(e) => setcapacity(e.target.value)} id="capacity"></Input>
 
-              <SheetClose asChild>
-                <Button type='submit' variant="outline" className="bg-slate-200 mt-4 w-full">Add Battery</Button>
-              </SheetClose>
-            </form>
-          </SheetHeader>
-        </SheetContent>
-      </Sheet>
-      <Button className="bg-slate-200 text-color-black" variant="outline" onClick={() => router.push(`pit`)}>Pit View</Button>
+                  <SheetClose asChild>
+                    <Button type='submit' variant="outline" className="bg-slate-200 mt-4 w-full">Add Battery</Button>
+                  </SheetClose>
+                </form>
+              </SheetHeader>
+            </SheetContent>
+          </Sheet>
+          <Button className="bg-slate-100 text-color-black hidden md:block" variant="outline" onClick={() => router.push(`pit`)}>Pit View</Button>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button className="bg-slate-100 text-color-black" variant="outline">Add User</Button>
+            </SheetTrigger>
+            <SheetContent className="p-3">
+              <SheetTitle className="text-center">Add User</SheetTitle>
+              <form>
+                <Label className="mt-3 mb-2" htmlFor="firstName">First Name:</Label>
+                <Input id="firstName" type="text" required></Input>
+                <Label className="mt-3 mb-2" htmlFor="lastName">Last Name:</Label>
+                <Input id="lastName" type="text" required></Input>
+                <Label className="mt-3 mb-2" htmlFor="username">Username:</Label>
+                <Input className="mb-5" id="username" type="text" required></Input>
+                <Label className="mt-3 mb-2" htmlFor="userRole">Role:</Label>
+                <RadioGroup defaultValue="member" className="max-w-sm" id="userRole">
+                  <FieldLabel htmlFor="admin">
+                    <Field orientation="horizontal">
+                      <FieldContent>
+                        <FieldTitle><Crown></Crown>Pit Admin</FieldTitle>
+                        <FieldDescription>
+                          Can manange batteries and users
+                        </FieldDescription>
+                      </FieldContent>
+                      <RadioGroupItem value="admin" id="admin" />
+                    </Field>
+                  </FieldLabel>
+                  <FieldLabel htmlFor="member">
+                    <Field orientation="horizontal">
+                      <FieldContent>
+                        <FieldTitle><Wrench></Wrench>Pit Member</FieldTitle>
+                        <FieldDescription>Can manange batteries</FieldDescription>
+                      </FieldContent>
+                      <RadioGroupItem value="member" id="member" />
+                    </Field>
+                  </FieldLabel>
+                </RadioGroup>
+                <SheetClose>
+                  <Button className="mt-5 bg-red-300 text-slate-100 w-full" variant="outline" type="submit">Add User</Button>
+                </SheetClose>
+              </form>
+            </SheetContent>
+          </Sheet>
+        </div>
+      </Card>
       {
         keys.map((key, index) => (
           <BatteryCard key={index} battery={key} />
